e827bfc8c97706ac2bee75bdcfd75f5d407b5b45
Borhan-svg <63272347+Borhan-svg@users.noreply.github.com>
2025-02-18 20:31:17 +0100

    adding screens

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:messenger_chat_head/screens/conversations_screen.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        const ConversationsScreen(),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: const Text('OnBoarding Screen'),
+        ),
+      ),
+    );
+  }
+}
